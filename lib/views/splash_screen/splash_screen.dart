276731ca5bf697e7df68df69ccbbb9b7010f96d4
HP <fmsohag34@gmail.com>
2023-07-17 20:19:57 +0600

    Test1

--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:e_commerce_app/const/colors.dart';
+import 'package:e_commerce_app/const/consts.dart';
+import 'package:e_commerce_app/const/images.dart';
+import 'package:e_commerce_app/widgets/applogo_widget.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -13,6 +16,24 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: redColor,
+      body: Center(
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topLeft,
+                  child: Image.asset(icSplashBg,width: 300,)),
+            20.heightBox,
+            applogoWidget(),
+            10.heightBox,
+            appname.text.fontFamily(bold).size(22).white.make(),
+            10.heightBox,
+            appversion.text.white.make(),
+            Spacer(),
+            credits.text.white.fontFamily(semibold).make(),
+            30.heightBox
+          ],
+        ),
+      ),
     );
   }
 }
